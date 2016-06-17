b2482f2ee87b720fa7bcf9a71a43364d4378d1ff
Viacheslav Karnaukh <viacheslav@ezlo.com>
2016-06-17 12:48:23 +0300

    created avatar page

--- app/index.js
+++ app/index.js
@@ -1,12 +1,49 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
+//https://api.github.com/users/viacheslav-karnaukh
+var USER_DATA = {
+	img: 'https://avatars.githubusercontent.com/u/7796067',
+	name: 'Viacheslav Karnaukh',
+	username: 'viacheslav-karnaukh'
+};
+
+var ProfilePic = React.createClass({
 	render: function() {
 		return (
-			<div>Hello World</div>
+			<img src={this.props.imgUrl} style={{height: 150, width: 150}} />
 		);
 	}
 });
 
-ReactDOM.render(<Hello/>, document.getElementById('app'));
+var ProfileLink = React.createClass({
+	render: function() {
+		return (
+			<a href={'https://github.com/' + this.props.username}>
+				{this.props.username}
+			</a>
+		);
+	}
+});
+
+var ProfileName = React.createClass({
+	render: function() {
+		return (
+			<div>{this.props.name}</div>
+		);
+	}
+});
+
+var Avatar = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<ProfilePic imgUrl={this.props.user.img} />
+				<ProfileName name={this.props.user.name} />
+				<ProfileLink username={this.props.user.username} />
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
